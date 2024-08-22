59076ba52f2f15d293d48ffa64db0172e5b0b78d
Phạm Hưng <phamhung7603@gmail.com>
2024-08-22 21:44:33 +0700

    part 2

--- bai1.php
+++ bai1.php
@@ -1,13 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php 
-        echo "hello world!"
+<?php 
+        function mytest(){
+            static $x = 0;
+            echo $x;
+            $x ++;
+        }
+        mytest();
+        mytest();
     ?>
-</body>
-</html>
